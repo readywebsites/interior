f7a547692915b339a4bb06a03d63f258cd2c4e4c
readywebsites <biz499websites@gmail.com>
2024-09-24 12:08:51 +0530

    Databases changes

--- vastukar/settings.py
+++ vastukar/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'vastukar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Interior',
+        'USER': 'Interior-1',
+        'PASSWORD': '123vastukaraA@A',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
